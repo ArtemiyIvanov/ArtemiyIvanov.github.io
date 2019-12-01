42bef791e5b752484321a8f3dfff35a840498f96
ArtemiyIvanov <ivanov_artemiy_pavlovich@mail.ru>
2019-12-01 21:35:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -21,10 +21,6 @@ $('.sl').slick({
     }
   ]
 });
-$(window.onpopstate = function(event) {
-  $('.overlay_popup, .popup').hide();
-  history.back();
-})
 $('.show_popup').click(function() { 
     var popup_id = $('#' + $(this).attr("rel")); 
     $(popup_id).show();
